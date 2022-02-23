52e08d93a9f152f0fae8108011ec1f70199d2c38
Tiago Santi <tiagosanti2009@gmail.com>
2022-02-23 11:23:21 -0400

    Classe Person adicionada

---
+++ FaceRecCustom/Person.cs
@@ -0,0 +1,42 @@
+﻿using FaceRecognitionDotNet;
+
+namespace FaceDetectionCustom
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public List<FaceEncoding>? FaceEncodings { get; set; }
+
+        public Person(string name)
+        {
+            Name = name;
+        }
+
+        public void AddEncoding(FaceEncoding encoding)
+        {
+            if (FaceEncodings == null)
+            {
+                FaceEncodings = new List<FaceEncoding>();
+            }
+            FaceEncodings.Add(encoding);
+        }
+
+        public override string ToString()
+        {
+            return "Name: " + Name +
+                "\nEncoding count: " + FaceEncondingsCount();
+        }
+
+        public string FaceEncondingsCount()
+        {
+            if (FaceEncodings != null)
+            {
+                return FaceEncodings.Count.ToString();
+            }
+            else
+            {
+                return "" + 0;
+            }
+        }
+    }
+}
