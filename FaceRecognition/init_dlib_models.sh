0b3decb595b2a121e7d4927061e8552d8f78df5f
Andrea055 <vivanco11.ac@gmail.com>
2023-10-11 13:04:22 +0200

    Create all the folder structure in init

--- FaceRecognition/init_dlib_models.sh
+++ FaceRecognition/init_dlib_models.sh
@@ -4,3 +4,5 @@ git clone https://github.com/davisking/dlib-models.git
 mv dlib-models/* models/
 bzip2 -d models/*
 rm -rf dlib-models
+mkdir -p models/data/images/known/
+mkdir -p models/data/encodings/known/
